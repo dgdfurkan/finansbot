8464c4053d661d790d0f62378524ba979c2a0413
dgdfurkan <frkngndz60@gmail.com>
2025-08-04 00:11:13 +0300

    no message
    Signed-off-by: dgdfurkan <frkngndz60@gmail.com>

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
+if not TOKEN:
+    raise Exception("BOT_TOKEN environment variable is missing!")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Merhaba! Finans asistanı botuna hoş geldin! 🚀")
